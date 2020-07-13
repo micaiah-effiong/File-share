8d71467e01fa8d189c54efcd73d9951436410ee5
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-13 17:36:02 +0100

    code refactor. fix bug

--- controllers/files.js
+++ controllers/files.js
@@ -5,43 +5,48 @@ const {
   errorResponse,
   asyncHandler,
 } = require("../handlers/index");
+
 fileList().then(console.log);
-module.exports = function () {
-  return {
-    // endpoint controller functions goes here
-    getAllFiles: asyncHandler(async (req, res, next) => {
-      let data = await fileList();
-      res.json({
-        status: true,
-        data,
-      });
-    }),
 
-    getOneFile: asyncHandler(async (req, res, next) => {
-      let fileName = req.params.id;
-      res.json({
-        status: true,
-      });
-    }),
+module.exports = {
+  // endpoint controller functions goes here
+  getAllFiles: asyncHandler(async (req, res, next) => {
+    let data = await fileList();
+    res.json({
+      status: true,
+      data,
+    });
+  }),
 
-    uploadFile: asyncHandler(async (req, res, next) => {
-      // run express file upload to handle file uploading
-    }),
+  getOneFile: asyncHandler(async (req, res, next) => {
+    let fileName = req.params.id;
+    let data = await fileList();
+    let file = data.filter((item) => item.filename == fileName);
+    if (!file || file.length < 1) {
+      return next(errorResponse("Error: Resource not found", 404));
+    }
 
-    downloadFile: asyncHandler(async (req, res, next) => {
-      let fileName = req.params.id;
-      let filePath = path.resolve(__dirname, "..", "files", fileName);
-      res.download(filePath, fileName);
-    }),
+    res.json({
+      status: true,
+    });
+  }),
 
-    deleteFile: asyncHandler(async (req, res, next) => {
-      let fileName = req.params.id;
-      let filePath = path.resolve(__dirname, "..", "files", fileName);
-      console.log(filePath);
-      // await fsPromises.unlink(fileName);
-      res.json({
-        status: true,
-      });
-    }),
-  };
+  uploadFile: asyncHandler(async (req, res, next) => {
+    // run express file upload to handle file uploading
+  }),
+
+  downloadFile: asyncHandler(async (req, res, next) => {
+    let fileName = req.params.id;
+    let filePath = path.resolve(__dirname, "..", "files", fileName);
+    res.download(filePath, fileName);
+  }),
+
+  deleteFile: asyncHandler(async (req, res, next) => {
+    let fileName = req.params.id;
+    let filePath = path.resolve(__dirname, "..", "files", fileName);
+    await fsPromises.unlink(fileName);
+    res.json({
+      status: true,
+    });
+  }),
 };

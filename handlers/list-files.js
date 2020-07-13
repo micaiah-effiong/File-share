9fc10be246c5824c3a18d2cd5d0aad8c57119fde
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-13 21:45:11 +0100

    code comment

--- handlers/list-files.js
+++ handlers/list-files.js
@@ -3,22 +3,26 @@ const mime = require("mime");
 const { promises: fsPromises } = require("fs");
 const path = require("path");
 const appFolder = path.resolve(__dirname, "..", "files");
-console.log(appFolder);
 
-/*
+/**
  * @return Array of files in the file folder
  */
 module.exports = async function () {
-  let list = []; // array to hold new list
-  let folderItemsList = await fsPromises.readdir(appFolder); // return and array of all files and folders
+  // Create an array to hold new list
+  let list = [];
+
+  // get array of all files and folders
+  let folderItemsList = await fsPromises.readdir(appFolder);
 
   for (let i = 0; i < folderItemsList.length; i++) {
-    let _file = folderItemsList[i]; // file or folder name
+    // file or folder name
+    let _file = folderItemsList[i];
     let fullFilePath = path.resolve(appFolder, _file);
 
     // check if the current item is an actual file
     if (!!(await fsPromises.stat(fullFilePath)).isFile()) {
-      let fileDetails = await fsPromises.stat(fullFilePath); // get file stats
+      // get file stats
+      let fileDetails = await fsPromises.stat(fullFilePath);
 
       // create file details
       let { size, birthtime } = fileDetails;

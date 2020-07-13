ffe10ff90599d55d2eaacbd061c58a6d3e4f91db
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-13 12:49:08 +0100

    added util function which returns file and details

--- handlers/list-files.js
+++ handlers/list-files.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
 const { promises: fsPromises } = require("fs");
-const util = require("util");
 const path = require("path");
 const appFolder = path.resolve(__dirname, "..", "files");
 console.log(appFolder);
 
 module.exports = async function () {
+  let list = [];
   let folderItemsList = await fsPromises.readdir(appFolder);
-
-  return await folderItemsList.filter(async (filePath) => {
+  for (let i = 0; i < folderItemsList.length; i++) {
+    let filePath = folderItemsList[i];
     let fullFilePath = path.resolve(appFolder, filePath);
-    return !!(await fsPromises.stat(fullFilePath)).isFile();
-  });
+    if (!!(await fsPromises.stat(fullFilePath)).isFile()) {
+      let fileDetails = await fsPromises.stat(fullFilePath);
+      list.push({ filename: filePath, details: fileDetails });
+    }
+  }
+  return list;
 };

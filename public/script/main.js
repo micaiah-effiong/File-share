76647f1818d8fcd76c48f2dbe2d32827cbb4f4bd
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-18 11:18:10 +0100

    upload files async using xhr to track upload progress

--- public/script/main.js
+++ public/script/main.js
@@ -1,4 +1,10 @@
-// import moment from "./moment.min.js";
+console.log(
+  `%c File share app running %c App Started %c`,
+  "background:#35495e ; padding: 2px; border-radius: 3px 0 0 3px;  color: #fff",
+  "background:#41b883 ; padding: 2px; border-radius: 0 3px 3px 0;  color: #fff",
+  "background:transparent; padding: 2px"
+);
+
 let socket = io();
 socket.on("joined", (d) => {
   console.log(d, "joined");
@@ -8,6 +14,7 @@ new Vue({
   data: {
     search: "",
     files: [],
+    uploadPercent: 0,
   },
   created: async function () {
     let res = await fetch("/api/files");
@@ -19,5 +26,29 @@ new Vue({
       let files = await res.json();
       this.files = files.data;
     });
+  },
+  methods: {
+    upload: async function (event) {
+      let self = this;
+      let file = new FormData(event.target.form);
+
+      let ajax = new XMLHttpRequest();
+      ajax.open("POST", "/api/files", true);
+
+      ajax.upload.onprogress = (event) => {
+        let { total, loaded, lengthComputable } = event;
+        let val = Math.round((loaded / total) * 100);
+        if (lengthComputable) {
+          self.uploadPercent = val;
+        }
+
+        if (val === 100) {
+          setTimeout(() => {
+            self.uploadPercent = 0;
+          }, 1000);
+        }
+      };
+      ajax.send(file);
+    },
   },
 });

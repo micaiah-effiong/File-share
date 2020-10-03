71fdbcdaffb4c5182258e1cd8a14543bc267aa97
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-03 21:27:00 +0100

    expose local-ip module

--- handlers/index.js
+++ handlers/index.js
@@ -1,9 +1,11 @@
 const asyncHandler = require("./async-handler");
 const errorResponse = require("./error-response");
 const listFiles = require("./list-files");
+const localIp = require("./local-ip");
 
 module.exports = {
   asyncHandler,
   errorResponse,
   listFiles,
+  localIp,
 };

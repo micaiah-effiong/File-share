6bad9b20c5cc0cf8a215e8a890620dd2afe98635
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-05 15:13:25 +0100

    added peer-server to socket-IO for signaling of peer

--- io.js
+++ io.js
@@ -2,6 +2,7 @@ let { server } = require("./app");
 let fs = require("fs");
 let path = require("path");
 let io = require("socket.io")(server);
+const peerIO = require("socket.io")(server, { path: "/p2p" });
 const { listFiles: fileList } = require("./handlers/index");
 
 let appFiles = [];
@@ -19,12 +20,26 @@ fs.watch(path.resolve(__dirname, "files"), async (err, data) => {
   }
 });
 
-io.on("connection", function (client) {
-  io.emit("JOINED", client.id);
+io.on("connection", function (socket) {
+  socket.emit("JOINED", socket.id);
+  console.log("client connected", socket.id); //log client connect
 
-  client.on("disconnect", function (id) {
+  socket.on("disconnect", function (id) {
     console.log("client disconnected", id); //log client disconnect
   });
 });
 
+peerIO.on("connection", function (socket) {
+  socket.broadcast.emit("JOINED", socket.id);
+  console.log("P client connected", socket.id); //log client connect
+
+  socket.on("PEER::CONNECTION", (peerId) => {
+    console.log("P peer connected", peerId); //log client connect
+    peerIO.emit("PEER::CONNECTION", peerId);
+  });
+
+  socket.on("disconnect", function (id) {
+    console.log("P client disconnected", id); //log client disconnect
+  });
+});
 module.exports = io;

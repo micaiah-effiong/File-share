1aab5dd089cfe9b1e6519ede12bf52d04ebd134e
Micah Effiong <micaiah.effiong@gmail.com>
2021-10-21 12:16:19 +0100

    code porting

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <!-- <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -27,7 +27,102 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-  </div>
+  </div> -->
+    <div id="app">
+      <div class="pb-3">
+        <div class="container top-nav" >
+          <header><br /></header>
+          <div class="form-group">
+            <input
+              type="search"
+              @input="searchFile($event.target.value)"
+              class="form-control"
+              placeholder="Search for files"
+            />
+          </div>
+          <div class="form-group nav" style="width: fit-content; margin: auto;">
+            <button
+              class="btn btn-link btn-sm mb-2"
+              @click="appEvent.$emit('downloadMultiple')"
+              title="download marked files"
+            >
+              <span class="fa fa-download"></span>
+            </button>
+            <button
+              @click="sortFiles()"
+              class="btn btn-link btn-sm mb-2 ml-1 mr-1"
+              title="show all files"
+            >
+              <span class="">All</span>
+            </button>
+            <button
+              @click="sortFiles('IMAGE')"
+              class="btn btn-link btn-sm mb-2 ml-1 mr-1"
+              title="photo"
+            >
+              <span class="fa fa-image"></span>
+            </button>
+            <button
+              class="btn btn-link btn-sm mb-2 ml-1 mr-1"
+              disabled
+            >
+              <span class=""></span>
+            </button>
+            <button
+              @click="sortFiles('AUDIO')"
+              class="btn btn-link btn-sm mb-2 ml-1 mr-1"
+              title="audio"
+            >
+              <span class="fa fa-music"></span>
+            </button>
+            <button
+              @click="sortFiles('VIDEO')"
+              class="btn btn-link btn-sm mb-2 ml-1 mr-1"
+              title="video"
+            >
+              <span class="fa fa-video-camera"></span>
+            </button>
+            <button
+              @click="sortFiles('FILES')"
+              class="btn btn-link btn-sm mb-2 ml-1 mr-1"
+              title="files"
+            >
+              <span class="fa fa-file"></span>
+            </button>
+          </div>
+          <form
+            method="POST"
+            action="/api/files"
+            enctype="multipart/form-data"
+            class="form"
+          >
+            <label for="upload" class="app-upload-btn"><span class="fa fa-upload fa-2"></span></label>
+            <input
+              type="file"
+              class="btn"
+              name="upload"
+              id="upload"
+              multiple
+              @change.prevent="upload"
+              hidden
+            />
+          </form>
+          <div v-if="uploadPercent > 0" style="color: white">{{uploadPercent}}%</div>
+        </div>
+        <div class="container-fluid">
+          <file-block></file-block>
+        </div>
+      </div>
+      
+      <footer class="card-footer app-footer">
+        <div class="row">
+          <div class="col-3">
+            {{numberOfFiles}} <span class="fa fa-file"></span>
+          </div>
+          <div class="col-9 text-right"></div>
+        </div>
+      </footer>
+    </div>
 </template>
 
 <script>

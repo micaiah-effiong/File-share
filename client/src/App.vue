d748eb5155a33bf6e4277038d131a48d23f5b469
Emilsone <lawsonezekiel68@gmail.com>
2021-10-22 14:36:52 -0700

    create a fixed header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <Navbar />
-  <UploadPage />
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <UploadPage />
+  </main>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import UploadPage from './components/UploadPage.vue'
+import Navbar from "./components/Navbar.vue";
+import UploadPage from "./components/UploadPage.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar,
-    UploadPage
-  }
-}
+    UploadPage,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Rubik&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Rubik&display=swap");
 #app {
-  background-color:#F1F1F3;
-font-family: 'Rubik', sans-serif;
+  background-color: #f1f1f3;
+  font-family: "Rubik", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

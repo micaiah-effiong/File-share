526a6cf9d2fc020344f967b8eb37dcfd1296b128
Micah Effiong <micaiah.effiong@gmail.com>
2022-03-27 19:19:11 +0100

    ⬆️ upgrade frontent
    - Setup vite bunder for faster start up
    - ⚡️increase productivity through reduced startup time
    - 🏷️Enabled typescript config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],

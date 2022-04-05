fcabfe30522c90190551e8677db260a688f38f60
Micah Effiong <micaiah.effiong@gmail.com>
2022-04-06 00:40:35 +0100

    💄 setup collors
    added tailwind colors and basic custom colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   mode: "jit",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      ash: {
+        light: "#86868a",
+        dark: "#1d1b22",
+        accent: "#66666b",
+        normal: "#26262e",
+      },
+    },
   },
   plugins: [],
 };

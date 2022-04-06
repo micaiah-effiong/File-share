f2e449a9b1efc97e360862f06c76d3c99076f862
Micah Effiong <micaiah.effiong@gmail.com>
2022-04-06 01:00:58 +0100

    ♻️ extend tailwind colors
    use extend to add custom colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,20 @@
-const colors = require("tailwindcss/colors");
 module.exports = {
   mode: "jit",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      ...colors,
-      ash: {
-        light: "#86868a",
-        dark: "#1d1b22",
-        accent: "#66666b",
-        normal: "#26262e",
+    extend: {
+      colors: {
+        ash: {
+          light: "#86868a",
+          dark: "#1d1b22",
+          accent: "#66666b",
+          normal: "#26262e",
+          secondary: "#34343b",
+        },
       },
+    },
+    colors: {
+      // ...colors,
     },
   },
   plugins: [],

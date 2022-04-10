6aac616afe1f09490d05584591d87121f91b8abf
Micah Effiong <micaiah.effiong@gmail.com>
2022-04-10 22:30:14 +0100

    💄 configure style colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   mode: "jit",
+  darkClass: "dark",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -11,10 +12,17 @@ module.exports = {
           normal: "#26262e",
           secondary: "#34343b",
         },
+        "ocean-blue": {
+          light: "#f5f9fd",
+          dark: "#4d788f",
+          accent: "#e8f0f7",
+          normal: "#078dee",
+          // secondary: "#81c5f6",
+          // "":"#668b9e"
+          // #e8f0f7
+          secondary: "#beccd7",
+        },
       },
-    },
-    colors: {
-      // ...colors,
     },
   },
   plugins: [],

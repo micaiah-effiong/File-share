921bc1ded8d5a046377089907fd9e4eb45f9aadc
Micah Effiong <micaiah.effiong@gmail.com>
2022-04-21 02:36:25 +0100

    🔧 configure storybook vite builder and postcss

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,8 +1,38 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    // "storybook-addon-jsx",
+
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
   framework: "@storybook/vue3",
-  features: {
-    postcss: false,
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+
+  // async viteFinal(config, { configType }) {
+  //   // customize the Vite config here
+  //   config.plugins = [
+  //     ...config.plugins,
+  //     require("@vitejs/plugin-vue").default(),
+  //     require("@vitejs/plugin-vue-jsx").default({
+  //       include: [/\.tesx$/, /\.[jt]sx$/],
+  //     }),
+  //   ];
+  //   return config;
+  // },
+
+  typescript: {
+    check: false,
+    checkOptions: {},
   },
 };

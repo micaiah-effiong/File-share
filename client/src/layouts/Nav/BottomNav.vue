0218c60e1b946e1e20cca6bd3c5ce81e6db470db
Micah Effiong <micaiah.effiong@gmail.com>
2022-04-26 01:04:28 +0100

    ✨ created buttom nav for small devices

---
+++ client/src/layouts/Nav/BottomNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="h-20 w-full px-4 py-2">
+    <div class="w-full h-full flex">
+      <BottomNavMenuItem>
+        <HomeIcon />
+      </BottomNavMenuItem>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  FolderIcon,
+  HomeIcon,
+  VideoCameraIcon,
+  PhotographIcon,
+  CogIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/vue/outline";
+import BottomNavMenuItem from "../../components/NavMenuItem/BottomNavMenuItem.vue";
+
+export default defineComponent({
+  components: { BottomNavMenuItem, HomeIcon },
+});
+</script>

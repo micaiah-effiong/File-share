ac61c1c03a10960383ba124e06ab825748199239
Micah Effiong <micaiah.effiong@gmail.com>
2022-04-26 01:05:59 +0100

    ✨ added wrapper to support bottom nav

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,146 +1,165 @@
 <template>
-  <div class="w-full h-full flex gap-14 dark bg-ocean-blue-light">
-    <Nav />
-    <main class="h-full w-full py-7 gap-8 flex flex-col px-4 md:px-0">
-      <header class="">
-        <div class="flex justify-between gap-4 text-ocean-blue-dark">
-          <RssIcon class="w-5" />
-          <div class="flex-1">
-            <input
-              class="w-full outline-none bg-transparent"
-              type="search"
-              name=""
-              id=""
-            />
-          </div>
-          <div class="flex gap-5">
-            <PlusCircleIcon class="w-5" />
-            <SearchIcon class="w-5" />
-            <StarIcon class="w-5" />
-          </div>
-        </div>
-        <div class="py-5">
-          <div class="cut"></div>
-        </div>
-        <nav>
-          <div class="flex justify-between text-ocean-blue-dark">
-            <div class="flex gap-2 md:gap-6 flex-1">
-              <div class="flex gap-2 shadow-md p-2 rounded-lg bg-white">
-                <FolderIcon class="w-5 text-ocean-blue-normal" />
-                <ChevronDownIcon class="w-3" />
-              </div>
-              <button>Recent Files</button>
-            </div>
-
-            <div class="flex items-center">
-              <div
+  <div class="flex flex-col h-full w-full justify-between">
+    <div class="w-full h-full flex gap-14 bg-ocean-blue-light">
+      <Nav />
+      <main class="h-full w-full py-7 gap-8 flex flex-col px-4 md:px-0">
+        <header class="">
+          <div class="flex justify-between gap-4 text-ocean-blue-dark">
+            <RssIcon class="w-5" />
+            <div class="flex-1">
+              <input
                 class="
-                  flex
-                  justify-between
-                  py-1
-                  px-2
-                  h-auto
-                  rounded-md
-                  bottom-2
-                  border-black
-                  gap-4
-                  bg-ocean-blue-accent
-                  shadow-ms shadow-inner
+                  w-full
+                  outline-none
+                  bg-transparent
+                  text-sm
+                  align-baseline
                 "
-              >
-                <button>
-                  <MenuIcon class="text-xs w-5" />
-                </button>
-                <button class="shadow-md rounded-[4px] bg-white py-1 px-1">
-                  <ViewBoardsIcon class="text-md w-5" />
-                </button>
-                <button>
-                  <ViewGridIcon class="text-xs w-5" />
-                </button>
-              </div>
+                type="search"
+                name=""
+                id=""
+                placeholder="search /"
+              />
+            </div>
+            <div class="flex gap-5">
+              <PlusCircleIcon class="w-5" />
+              <SearchIcon class="w-5" />
+              <StarIcon class="w-5" />
             </div>
           </div>
-        </nav>
-      </header>
-      <main>
-        <div>
-          <div
-            class="
-              bg-white
-              md:w-4/12
-              w-1/2
-              rounded-lg
-              shadow-sm
-              mono:bg-ash-normal
-              mono:text-ash-accent
-              mono:hover:bg-ash-secondary
-              mono:hover:transition-colors
-              transition-colors
-            "
-          >
-            <div class="grid w-full p-3 rounded-md">
-              <div>
-                <div class="flex justify-between py-1">
-                  <button>
-                    <StarIcon class="w-5 text-ocean-blue-secondary" />
-                  </button>
-                  <button>
-                    <DotsVerticalIcon
-                      class="w-5 mono:text-ash-light text-ocean-blue-secondary"
-                    />
-                  </button>
+          <div class="py-5">
+            <div class="cut"></div>
+          </div>
+          <nav>
+            <div class="flex justify-between text-ocean-blue-dark">
+              <div class="flex gap-2 md:gap-6 flex-1">
+                <div class="flex gap-2 shadow-md p-2 rounded-lg bg-white">
+                  <FolderIcon class="w-5 text-ocean-blue-normal" />
+                  <ChevronDownIcon class="w-3" />
                 </div>
-                <div class="grid gap-3 py-3 justify-center text-center">
-                  <div class="text-[4rem] flex justify-center">
-                    <span>
-                      <DocumentIcon class="h-16 text-ocean-blue-secondary" />
-                    </span>
-                  </div>
-                  <div class="text-[0.8rem] text-ocean-blue-dark font-semibold">
-                    file-name.png
-                  </div>
-                </div>
+
+                <button class="hidden align-middle md:block">
+                  Recent Files
+                </button>
               </div>
 
-              <!-- <div class="cut"></div> -->
-
-              <div class="flex pt-3 pb-2 cut justify-between items-center">
-                <div class="uppercase text-xs text-ocean-blue-dark">2mb</div>
+              <div class="flex items-center">
+                <div
+                  class="
+                    flex
+                    justify-between
+                    py-1
+                    px-2
+                    h-auto
+                    rounded-md
+                    bottom-2
+                    border-black
+                    gap-4
+                    bg-ocean-blue-accent
+                    shadow-ms shadow-inner
+                  "
+                >
+                  <button>
+                    <MenuIcon class="text-xs w-5" />
+                  </button>
+                  <button class="shadow-md rounded-[4px] bg-white py-1 px-1">
+                    <ViewBoardsIcon class="text-md w-5" />
+                  </button>
+                  <button>
+                    <ViewGridIcon class="text-xs w-5" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <div>
+            <div
+              class="
+                bg-white
+                md:w-4/12
+                w-1/2
+                rounded-lg
+                shadow-sm
+                mono:bg-ash-normal
+                mono:text-ash-accent
+                mono:hover:bg-ash-secondary
+                mono:hover:transition-colors
+                transition-colors
+              "
+            >
+              <div class="grid w-full p-3 rounded-md">
                 <div>
-                  <div
-                    class="
-                      outline-1 outline-white
-                      bg-red-500
-                      w-6
-                      h-6
-                      rounded-md
-                      flex
-                      justify-center
-                      items-center
-                      text-sm
-                      font-semibold
-                      text-white
-                      p-1
-                    "
-                  >
-                    <span>O</span>
+                  <div class="flex justify-between py-1">
+                    <button>
+                      <StarIcon class="w-5 text-ocean-blue-secondary" />
+                    </button>
+                    <button>
+                      <DotsVerticalIcon
+                        class="
+                          w-5
+                          mono:text-ash-light
+                          text-ocean-blue-secondary
+                        "
+                      />
+                    </button>
+                  </div>
+                  <div class="grid gap-3 py-3 justify-center text-center">
+                    <div class="text-[4rem] flex justify-center">
+                      <span>
+                        <DocumentIcon class="h-16 text-ocean-blue-secondary" />
+                      </span>
+                    </div>
+                    <div
+                      class="text-[0.8rem] text-ocean-blue-dark font-semibold"
+                    >
+                      file-name.png
+                    </div>
+                  </div>
+                </div>
+
+                <!-- <div class="cut"></div> -->
+
+                <div class="flex pt-3 pb-2 cut justify-between items-center">
+                  <div class="uppercase text-xs text-ocean-blue-dark">2mb</div>
+                  <div>
+                    <div
+                      class="
+                        outline-1 outline-white
+                        bg-red-500
+                        w-6
+                        h-6
+                        rounded-md
+                        flex
+                        justify-center
+                        items-center
+                        text-sm
+                        font-semibold
+                        text-white
+                        p-1
+                      "
+                    >
+                      <span>O</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <!-- <div class="grid p-2 gap-1 h-[10rem]">
+          <!-- <div class="grid p-2 gap-1 h-[10rem]">
           <div class="bg-[#81c5f6]">secondary</div>
           <div class="bg-[#e8f0f7]">accent</div>
           <div class="bg-[#078dee]">normal</div>
           <div class="bg-[#4d788f]">dark</div>
           <div class="bg-[#f5f9fd]">bg light</div>
         </div> -->
+        </main>
       </main>
-    </main>
-    <FilePreview />
+      <FilePreview />
+    </div>
+    <BottomNav />
   </div>
 </template>
 
@@ -161,11 +180,13 @@ import {
 import { DocumentIcon, FolderIcon } from "@heroicons/vue/solid";
 import "./App.css";
 import Nav from "./layouts/Nav/Nav.vue";
+import BottomNav from "./layouts/Nav/BottomNav.vue";
 import FilePreview from "./layouts/FilePreview/FilePreview.vue";
 
 export default defineComponent({
   components: {
     Nav,
+    BottomNav,
     FilePreview,
     FolderIcon,
     MenuIcon,

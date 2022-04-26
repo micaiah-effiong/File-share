76e5ac9c8641ccb17370023865926a7171f83814
Micah Effiong <micaiah.effiong@gmail.com>
2022-04-26 01:03:49 +0100

    ✨ created a buttom nav item

---
+++ client/src/components/NavMenuItem/BottomNavMenuItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    class="
+      grid
+      justify-center justify-items-center
+      items-center
+      text-ocean-blue-dark
+    "
+  >
+    <div class="nav-item px-3">
+      <div class="w-6">
+        <slot></slot>
+      </div>
+    </div>
+
+    <span class="text-xs">Home</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>

749f6deb640844262be3b8939f48998327352b3f
Micah Effiong <micaiah.effiong@gmail.com>
2022-05-01 23:10:55 +0100

    💄 added a close icon

--- client/src/layouts/FilePreview/FilePreview.vue
+++ client/src/layouts/FilePreview/FilePreview.vue
@@ -1,46 +1,62 @@
 <template>
-  <aside className="h-full max-w-5/12 w-5/12 py-7 bg-white hidden md:block">
-    <div className="flex flex-col h-full ">
-      <header className="px-3">
-        <div className="flex gap-2 text-ocean-blue-dark">
-          <DocumentTextIcon class="w-5" />
-          <span>File Preview</span>
+  <aside class="h-full max-w-5/12 w-5/12 py-7 bg-white hidden md:block">
+    <div class="flex flex-col h-full">
+      <header class="px-3">
+        <div class="flex justify-between text-ocean-blue-dark">
+          <div class="flex gap-2">
+            <DocumentTextIcon class="w-5" />
+            <span>File Preview</span>
+          </div>
+          <button class="w-5"><XIcon /></button>
         </div>
-        <div className="py-5">
-          <div className="cut"></div>
+        <div class="py-5">
+          <div class="cut"></div>
         </div>
       </header>
-      <main className="h-full grid overflow-auto">
-        <main className="px-10 text-sm text-ocean-blue-dark">
-          <div className="grid gap-7">
-            <div className="grid gap-3">
-              <div className="flex justify-center">
+      <main class="h-full grid overflow-auto">
+        <main class="px-10 text-sm text-ocean-blue-dark">
+          <div class="grid gap-7">
+            <div class="grid gap-3">
+              <div class="flex justify-center">
                 <DocumentIcon class="h-48 text-ocean-blue-secondary" />
               </div>
-              <div className="grid gap-2">
-                <div className="font-semibold break-all">
+              <div class="grid gap-2">
+                <div class="font-semibold break-all">
                   users-file-screenshots-image-media-somefile.png
                 </div>
-                <div className="text-xs">2.5GB</div>
+                <div class="text-xs">2.5GB</div>
               </div>
             </div>
-            <div className="cut"></div>
-            <div className="break-words grid gap-2">
-              <div className="font-semibold">Description</div>
-              <div className="text-xs leading-5">
+            <div class="cut"></div>
+            <div class="break-words grid gap-2">
+              <div class="font-semibold">Description</div>
+              <div class="text-xs leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                 tempus suscipit dolor tincidunt gravida. Praesent ac rutrum
                 massa. Cras vestibulum magna a tortor varius, nec interdum dui
                 dapibus.
               </div>
             </div>
-            <div className="cut"></div>
-            <div className="grid gap-2">
-              <div className="font-semibold">Shared with:</div>
-              <div className="grid">
-                <div className="flex gap-3 items-center">
+            <div class="cut"></div>
+            <div class="grid gap-2">
+              <div class="font-semibold">Shared with:</div>
+              <div class="grid">
+                <div class="flex gap-3 items-center">
                   <div
-                    className="outline-1 outline-white bg-red-500 w-6 h-6 rounded-md flex justify-center items-center text-sm font-semibold text-white p-1 "
+                    class="
+                      outline-1 outline-white
+                      bg-red-500
+                      w-6
+                      h-6
+                      rounded-md
+                      flex
+                      justify-center
+                      items-center
+                      text-sm
+                      font-semibold
+                      text-white
+                      p-1
+                    "
                   >
                     <span>S</span>
                   </div>
@@ -50,30 +66,22 @@
             </div>
           </div>
         </main>
-        <div className="border-t self-end mt-7 py-5 px-10">
-          <div
-            className="flex justify-between text-xs text-ocean-blue-dark gap-3"
-          >
-            <div className="grid gap-3 justify-items-center">
-              <div
-                className="w-6 text-white bg-ocean-blue-normal rounded-md p-1"
-              >
+        <div class="border-t self-end mt-7 py-5 px-10">
+          <div class="flex justify-between text-xs text-ocean-blue-dark gap-3">
+            <div class="grid gap-3 justify-items-center">
+              <div class="w-6 text-white bg-ocean-blue-normal rounded-md p-1">
                 <ExternalLinkIcon />
               </div>
               <div>Share</div>
             </div>
-            <div className="grid gap-3 justify-items-center">
-              <div
-                className="w-6 text-white bg-ocean-blue-normal rounded-md p-1"
-              >
+            <div class="grid gap-3 justify-items-center">
+              <div class="w-6 text-white bg-ocean-blue-normal rounded-md p-1">
                 <PencilIcon />
               </div>
               <div>Edit</div>
             </div>
-            <div className="grid gap-3 justify-items-center">
-              <div
-                className="w-6 text-white bg-ocean-blue-normal rounded-md p-1"
-              >
+            <div class="grid gap-3 justify-items-center">
+              <div class="w-6 text-white bg-ocean-blue-normal rounded-md p-1">
                 <TrashIcon />
               </div>
               <div>Delete</div>
@@ -93,6 +101,7 @@ import {
   ExternalLinkIcon,
   PencilIcon,
   TrashIcon,
+  XIcon,
 } from "@heroicons/vue/outline";
 
 export default defineComponent({
@@ -102,6 +111,7 @@ export default defineComponent({
     PencilIcon,
     TrashIcon,
     DocumentIcon,
+    XIcon,
   },
 });
 </script>

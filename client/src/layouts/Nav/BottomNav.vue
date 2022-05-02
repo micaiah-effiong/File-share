b6dfa71b68f50f104b237f7146343fc40d55e8bd
Micah Effiong <micaiah.effiong@gmail.com>
2022-05-02 18:53:14 +0100

    💄 wrap set component to fix position
    added custom style class to hide below page

--- client/src/layouts/Nav/BottomNav.vue
+++ client/src/layouts/Nav/BottomNav.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="h-full w-full px-4 py-2 relative bg-white">
-    <div class="w-full h-full flex justify-evenly">
-      <BottomNavMenuItem text="Home">
-        <HomeIcon />
-      </BottomNavMenuItem>
-      <BottomNavMenuItem text="Recent">
-        <FolderIcon />
-      </BottomNavMenuItem>
-      <BottomNavMenuItem text="video">
-        <VideoCameraIcon />
-      </BottomNavMenuItem>
-      <BottomNavMenuItem text="Photo">
-        <PhotographIcon />
-      </BottomNavMenuItem>
-      <!-- <BottomNavMenuItem text="Setting">
+  <div class="h-16 md:hidden fixed w-full bottom-0 bottom-nav">
+    <div class="h-full w-full px-4 py-2 relative bg-white">
+      <div class="w-full h-full flex justify-evenly">
+        <BottomNavMenuItem text="Home">
+          <HomeIcon />
+        </BottomNavMenuItem>
+        <BottomNavMenuItem text="Recent">
+          <FolderIcon />
+        </BottomNavMenuItem>
+        <BottomNavMenuItem text="video">
+          <VideoCameraIcon />
+        </BottomNavMenuItem>
+        <BottomNavMenuItem text="Photo">
+          <PhotographIcon />
+        </BottomNavMenuItem>
+        <!-- <BottomNavMenuItem text="Setting">
         <CogIcon />
       </BottomNavMenuItem> -->
+      </div>
     </div>
   </div>
 </template>
@@ -28,7 +30,6 @@ import {
   VideoCameraIcon,
   PhotographIcon,
   CogIcon,
-  DotsHorizontalIcon,
 } from "@heroicons/vue/outline";
 import BottomNavMenuItem from "../../components/NavMenuItem/BottomNavMenuItem.vue";
 
@@ -43,3 +44,12 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.bottom-nav {
+  transition: 0.3s bottom;
+}
+.hide-below {
+  bottom: -4rem;
+}
+</style>

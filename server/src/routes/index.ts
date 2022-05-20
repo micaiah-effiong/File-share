e8111dc36c43329cbceb1d495c9e80cc6b9cb29b
Micah Effiong <micaiah.effiong@gmail.com>
2022-05-20 15:58:19 +0100

    🗑️ clean endpoints for static file rendering

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,13 +6,4 @@ const router: Router = Router();
 router.use("/auth", authRouter);
 router.use("/api", apiRouter);
 
-/* GET home page. */
-router.get("/", function (req: Request, res: Response, next: NextFunction) {
-  res.render("dashboard");
-});
-
-router.get("/p2p", function (req, res, next) {
-  res.render("p2p");
-});
-
 export default router;

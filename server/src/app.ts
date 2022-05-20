7153a66dbd4487e45171f5ff90af51aa34b75f7a
Micah Effiong <micaiah.effiong@gmail.com>
2022-05-20 15:58:41 +0100

    🗑️ clean up code and handle errors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,7 @@
 import dotenv from "dotenv";
-import createError from "http-errors";
 import http from "http";
 import express, { Request, Response, NextFunction } from "express";
 import multer from "multer";
-import path from "path";
-// import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
 import { ExpressPeerServer } from "peer";
@@ -15,28 +12,26 @@ const app = express();
 /**
  * Create HTTP server.
  */
-
 const server = http.createServer(app);
 const peerServer = ExpressPeerServer(server, {
   // debug: true,
 });
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/peer", peerServer);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next: NextFunction) {
-  next(createError(404));
+app.use(function (_, res, next: NextFunction) {
+  return res.status(404).json({
+    success: false,
+    error: "NOT FOUND",
+    message: "Resource not found",
+    result: null,
+  });
 });
 
 // error handler
@@ -49,9 +44,13 @@ app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
     // A Multer error occurred when uploading.
   }
 
-  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    success: false,
+    error: err.name || "SERVER ERROR",
+    message: err.message || "",
+    result: null,
+  });
 });
 
 export { app, server, peerServer };

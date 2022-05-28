300067d1f3c5c649248266e6fe41cc3dbb1d9c39
Micah Effiong <micaiah.effiong@gmail.com>
2022-05-28 03:06:03 +0100

    🎨 completed upload logic

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,4 +1,4 @@
-import axios from "redaxios";
+import axios from "axios";
 
 export function debounce(cb: Function, delay: number): Function {
   let timeout: number;
@@ -24,6 +24,8 @@ export function throttle(cb: Function, delay: number): Function {
   };
 }
 
+const API_ORIGIN = "http://localhost:3300";
+
 export async function getFiles(): Promise<
   {
     filename: string;
@@ -36,6 +38,20 @@ export async function getFiles(): Promise<
     fileType: string;
   }[]
 > {
-  const res = await axios.get("http://localhost:3300/api/files");
+  const res = await axios.get(`${API_ORIGIN}/api/files`);
   return res.data.data;
+}
+
+export async function upload(
+  data: any,
+  options: {
+    onUploadProgress?: (progressEvent: any) => void;
+    [key: string]: any;
+  } = {}
+): Promise<any> {
+  options.headers = {
+    "Content-Type": "multipart/form-data",
+  };
+  const res = await axios.post(`${API_ORIGIN}/api/files`, data, options);
+  return res.data;
 }

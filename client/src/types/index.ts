5e5bbbb24ab8b98a8ac097d56abc8764eb0c074a
Micah Effiong <micaiah.effiong@gmail.com>
2022-07-07 01:11:05 +0100

    🏷️ created display-file type

---
+++ client/src/types/index.ts
@@ -0,0 +1,10 @@
+export type DisplayFile = {
+  filename: string;
+  size: string;
+  short: string;
+  createdAt: string;
+  link: string;
+  downloadLink: string;
+  streamLink: string;
+  fileType: string;
+};

18b436883e8dda9a596c1f6b9ea2b9e1dcdd8d25
Micah Effiong <micaiah.effiong@gmail.com>
2022-07-18 00:24:32 +0100

    🎨 implement simple custom useStorage

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,13 @@
 import { defineStore } from "pinia";
-import { useStorage } from "@vueuse/core";
+import { ref, watch } from "vue";
 import { DisplayFile } from "../types";
 import { getFiles } from "../utils";
-import { FileViewTypesList, RootActions, RootState } from "./types";
+import {
+  FileViewTypes,
+  FileViewTypesList,
+  RootActions,
+  RootState,
+} from "./types";
 
 export const useMainStore = defineStore<any, RootState, {}, RootActions>(
   "mainStore",
@@ -20,7 +25,10 @@ export const useMainStore = defineStore<any, RootState, {}, RootActions>(
       },
       allFetchedFiles: [],
       previewStatus: false,
-      fileViewType: useStorage<FileViewTypesList>("fileViewType", "GRID"),
+      fileViewType: useStorage<FileViewTypesList>(
+        "fileViewType",
+        FileViewTypes.SEMI_LIST
+      ),
     }),
     actions: {
       async fetchAllFiles() {
@@ -49,3 +57,41 @@ export const useMainStore = defineStore<any, RootState, {}, RootActions>(
     },
   }
 );
+
+function useStorage<T>(key: string, stateDate: T) {
+  const storedState = read(key);
+
+  const data = ref(storedState ?? stateDate);
+  write(key, data.value);
+
+  watch(
+    () => {
+      console.log("reading...");
+      return data.value;
+    },
+
+    (newState, oldState) => {
+      write(key, newState);
+      data.value = newState;
+      console.log("writing...", { newState, oldState });
+    }
+  );
+
+  console.log({ storedState, state: data.value });
+
+  function read(key: string) {
+    console.log("reading()...");
+    const storageData = localStorage.getItem(key);
+    return JSON.parse(JSON.stringify(storageData));
+  }
+
+  function write(key: string, payload: any) {
+    localStorage.setItem(
+      key,
+      typeof payload === "object" ? JSON.stringify(payload) : payload.toString()
+    );
+    console.log("writing()...", { key, payload });
+  }
+
+  return data;
+}

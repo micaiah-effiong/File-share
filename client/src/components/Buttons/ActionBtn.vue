71675992ec7436ede253856b829ec7f8cbcbcf39
Micah Effiong <micaiah.effiong@gmail.com>
2022-07-22 03:32:14 +0100

    🎨 setup disable state - change button color when disabled

--- client/src/components/Buttons/ActionBtn.vue
+++ client/src/components/Buttons/ActionBtn.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="grid gap-3 justify-items-center">
-    <div
+    <button
       role="button"
-      class="w-6 text-white bg-ocean-blue-normal rounded-md p-1"
+      class="
+        w-6
+        text-white
+        bg-ocean-blue-normal
+        disabled:bg-ocean-blue-dark
+        rounded-md
+        p-1
+      "
+      :disabled="isDisabled"
     >
       <slot></slot>
-    </div>
+    </button>
     <div>{{ label }}</div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { defineProps } from "vue";
-defineProps({
-  label: {
-    type: String,
-    required: true,
-  },
+withDefaults(defineProps<{ label: string; isDisabled?: boolean }>(), {
+  isDisabled: false,
 });
 </script>

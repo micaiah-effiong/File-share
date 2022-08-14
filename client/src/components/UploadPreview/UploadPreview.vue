e5bda1ab524e4d848a2afc7810106530d9e75d54
Micah Effiong <micaiah.effiong@gmail.com>
2022-08-14 01:43:15 +0100

    ♻️ remove small upload-preview
    added support for aborting upload
    
    display uploaded items with UploadFileItem components

--- client/src/components/UploadPreview/UploadPreview.vue
+++ client/src/components/UploadPreview/UploadPreview.vue
@@ -1,65 +1,56 @@
 <template>
 	<div
-		v-bind="$attrs"
-		v-if="size === 'MD' && (displayStatus === 'OPEN' || displayStatus === 'DONE')"
-		class="drop-shadow-md drop-shadow-ocean-blue-dark shadow-md shadow-ocean-blue-accent absolute bottom-0 right-5 lg:w-3/12 md:grid md:w-5/12 hidden rounded-sm h-auto bg-ocean-blue-light"
+		v-if="displayStatus !== 'CLOSE'"
+		class="drop-shadow-md drop-shadow-ocean-blue-dark shadow-md shadow-ocean-blue-accent absolute bottom-0 right-5 lg:w-3/12 md:flex hidden h-auto md:w-5/12 w-5/12 rounded-sm bg-ocean-blue-light"
 	>
-		<div class="grid text-ocean-blue-light bg-ocean-blue-mid-night p-2">
-			<div class="flex justify-between gap-2">
-				<div class="flex justify-between items-center w-full">
-					<div v-if="percentUploaded < 100">Uploading {{ uploaded }} of {{ total }}</div>
+		<div class="relative w-full">
+			<div class="grid text-ocean-blue-light bg-ocean-blue-mid-night p-2">
+				<div class="flex justify-between gap-2">
+					<div class="flex justify-between items-center w-full">
+						<div v-if="percentUploaded < 100">Uploading {{ uploaded }} of {{ total }}</div>
 
-					<div v-if="percentUploaded === 100">Uploading complete</div>
+						<div v-if="percentUploaded === 100">Uploading complete</div>
 
-					<div class="relative">
-						<CircleProgressVue :percent="percentUploaded" />
+						<div class="relative">
+							<CircleProgressVue :percent="percentUploaded" />
+						</div>
+					</div>
+					<div class="flex gap-2">
+						<button class="relative ml-auto" @click="showDetails = !showDetails">
+							<ChevronDownIcon class="w-3" />
+						</button>
+						<button class="right-2 top-2" @click="emit('close')"><XIcon class="w-4" /></button>
 					</div>
 				</div>
-				<div class="flex gap-2">
-					<button class="relative ml-auto" @click="showDetails = !showDetails">
-						<ChevronDownIcon class="w-3" />
-					</button>
-					<button class="right-2 top-2" @click="emit('close')"><XIcon class="w-4" /></button>
-				</div>
 			</div>
-		</div>
-		<div :class="showDetails ? 'grid' : 'hidden'">
-			<div
-				v-for="uploadingFile in uploadingFilesList"
-				class="p-2 text-sm flex gap-2 border-b border-ocean-blue-secondary"
-			>
-				<div class="truncate ...">{{ uploadingFile.filename }}</div>
-				<div class="ml-auto text-xs shrink-0 w-16">
-					{{ uploadingFile.progress < 100 ? `${uploadingFile.progress}%` : "Complete" }}
-				</div>
+			<div :class="showDetails ? 'flex flex-col' : 'hidden'">
+				<UploadFileItem
+					v-for="uploadingFile in uploadingFilesList"
+					:key="uploadingFile.filename"
+					:abortController="uploadingFile.abortController"
+					:filename="uploadingFile.filename"
+					:progress="uploadingFile.progress"
+				/>
 			</div>
 		</div>
 	</div>
-
-	<button
-		v-if="size === 'SM'"
-		@click="emit('close')"
-		class="text-ocean-blue-dark drop-shadow-md drop-shadow-ocean-blue-dark"
-		v-bind="$attrs"
-	>
-		<CircleProgressVue :percent="percentUploaded" />
-	</button>
 </template>
 
 <script lang="ts" setup>
 import { XIcon, ChevronDownIcon } from "@heroicons/vue/outline";
-import { ref, toRef, watch } from "vue";
+import { ref } from "vue";
+import { UploadFilesProgressDetails } from "../../types";
 import CircleProgressVue from "./CircleProgress.vue";
+import UploadFileItem from "./UploadFileItem.vue";
 
 const emit = defineEmits<{ (event: "close"): void }>();
-const props = withDefaults(
+withDefaults(
 	defineProps<{
 		uploaded: number;
 		total: number;
 		percentUploaded: number;
-		size: "SM" | "MD";
 		displayStatus?: "OPEN" | "DONE" | "CLOSE";
-		uploadingFilesList: Array<{ filename: string; progress: number }>;
+		uploadingFilesList: UploadFilesProgressDetails["uploadingFilesDetails"];
 	}>(),
 	{
 		total: 0,
@@ -71,10 +62,4 @@ const props = withDefaults(
 );
 
 const showDetails = ref(true);
-
-watch(toRef(props, "displayStatus"), () => {
-	if (props.displayStatus !== "OPEN" && props.size === "SM") {
-		emit("close");
-	}
-});
 </script>
